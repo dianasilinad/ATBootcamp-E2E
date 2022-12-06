fa3f47cb683eaef65c9d4c1638e85e90c9b96291
Diana Silina-Duljaninova <92679283+dianasilinad@users.noreply.github.com>
2022-12-06 17:58:03 +0200

    made changes in common file with trycatch function

--- features/step-definitions/common.js
+++ features/step-definitions/common.js
@@ -1,12 +1,21 @@
 const { Given, When, Then } = require('@wdio/cucumber-framework');
 
 Given("I am on the Home page", async () => {
-    await browser.url(`https://www.newegg.com`)
-    await expect(browser).toHaveUrl('https://www.newegg.com/')
-    await browser.deleteCookies()
+    await browser.refresh();
+    await browser.url(`https://www.newegg.com`);
+    await expect(browser).toHaveUrl('https://www.newegg.com/');
+    
 });
 Given("I close the banner", async () => {
-    $('#modal-Website > div.modal-dialog.modal-dialog-centered > div > div').waitForDisplayed
-    const closeTheBanner = await $('#modal-Website > div.modal-dialog.modal-dialog-centered > div > button')
-    closeTheBanner.click()
+    // $('#modal-Website > div.modal-dialog.modal-dialog-centered > div > div').waitForDisplayed
+    // const closeTheBanner = await $('#modal-Website > div.modal-dialog.modal-dialog-centered > div > button');
+    // closeTheBanner.click();
+    try {
+        let closeTheBanner=await $('#modal-Website > div.modal-dialog.modal-dialog-centered > div > button');
+        await closeTheBanner.waitForDisplayed()
+        closeTheBanner.click();
+      } catch (err) {
+        await browser.refresh();
+      }
 });
+
--- features/step-definitions/shopLogo.js
+++ features/step-definitions/shopLogo.js
@@ -1,15 +1,15 @@
 const { Given, When, Then } = require('@wdio/cucumber-framework');
 
 When('I enter the word "Windows" in the search bar',async () => {
-    const searchBar = await $("[title~=Search]")
-    searchBar.addValue('Windows')
+    const searchBar = await $("[title~=Search]");
+    searchBar.addValue('Windows');
     
 });
 Then('I click the search button', async () => {
     const searchButton = await $(".header2021-search-button");
-    searchButton.click()
+    searchButton.click();
 });
 Then('I check that at least one item appears', async () => {
     const someItem = await $("#item_cell_32-351-756_1_0 > div");
-    someItem.isDisplayed()
+    someItem.isDisplayed();
 });
